c23bc142c80a7ccecdf7c9850a80fdb8773705b1
Jesica Ramirez <jramireztoscano@uchicago.edu>
2020-07-03 18:35:06 -0500

    3rd week

--- handle_data.R
+++ handle_data.R
@@ -5,11 +5,13 @@ library(rgdal)
 library(dplyr)
 library(colorspace)
 library(htmltools)
-
+library(stringr)
+library(stringi)
+library(tidyverse)
 ## Download Shapes and Data ##
 shapes <- readOGR( "data/shapes/tl_2017_us_state.shp")
 hg <-read.csv('data/splc-hate-groups.csv')
-hc <-read.csv('data/ucr_hatecrimes.csv')
+agg_hc <-read.csv('data/agg_hc.csv') #use aggregated data from python code
 pop <-read.csv('data/population.csv')
 
 ## Aggregate Hate Groups Data ##
@@ -22,12 +24,13 @@ only_immigrant = filter(hg, hg['Ideology'] == 'Anti-Immigrant') %>%
   summarize(count_hg_i= n())
 
 hg_counts = merge(all_hg, only_immigrant, by=c('State', 'Year'), all=TRUE)
-hg_counts[is.na(hg_counts)] <- 0
 
 #Include population estimates
 hg_counts = merge(hg_counts, pop, by=c('State', 'Year'), all=TRUE)
 
 #Normalize number of hate groups per million people
+hg_counts$count_hg[is.na(hg_counts$count_hg)] <- 0
+hg_counts$count_hg_i[is.na(hg_counts$count_hg_i)] <- 0
 hg_counts['count_hg_n'] = hg_counts$count_hg / hg_counts$population * 1000000
 hg_counts['count_hg_i_n'] = hg_counts$count_hg_i / hg_counts$population * 1000000
 #Filter
@@ -35,20 +38,25 @@ hg_counts = filter(hg_counts, hg_counts['State'] != 'District of Columbia')
 all_states = tibble(unique(hg_counts$State))
 colnames(all_states) <-c('State')
 
-## Aggregate Hate Crimes Data ##
+## Merge with Hate Crimes Data ##
+colnames(agg_hc)
+hdata = merge(hg_counts, agg_hc, by=c('State', 'Year'), all=TRUE)
+hdata$race[is.na(hdata$race)] <- 0#Replace NA with 0
+hdata$latino[is.na(hdata$latino)] <- 0
+hdata['race_n'] = hdata$race / hdata$population * 1000000
+hdata['latino_n'] = hdata$latino / hdata$population * 1000000
 
-
+## Melt Data
 
 ## Merge all aggregated data with Shapes ##
 select_year = 2016
-df_year = filter(hg_counts, hg_counts['Year'] == select_year)[c('State', 'count_hg','count_hg_n',
-                                                                'count_hg_i','count_hg_i_n')]
+df_year = filter(hdata, hdata['Year'] == select_year)
 df_year =  merge(df_year, all_states, by='State', all=TRUE)
 df_year[is.na(df_year)] <- 0
 
 shapes_toplot = merge(shapes, df_year, 
                by.x='NAME', by.y='State', all=FALSE)
-pal2_df = data.frame(rainbow_hcl(50))
+#pal2_df = data.frame(rainbow_hcl(50))
 
 map_plot<-function(shapes_toplot, var_toplot, title, color="PuBu", bins=4){
   pal = colorBin(color, shapes_toplot[[var_toplot]], bins=bins)
@@ -75,3 +83,7 @@ map_plot<-function(shapes_toplot, var_toplot, title, color="PuBu", bins=4){
 
 map_plot(shapes_toplot, 'count_hg_n', 'Number of Hate Groups:', color='Reds')
 map_plot(shapes_toplot, 'count_hg_i_n', 'Number of Anti-Immigrant Groups:')
+map_plot(shapes_toplot, 'race_n', 'Number of Race Motivated Hate Crimes per Million:', color='Oranges')
+map_plot(shapes_toplot, 'latino_n', 'Number of Anti-Latino Motivated Hate Crimes per Million:', 
+         color='BuPu')
+
